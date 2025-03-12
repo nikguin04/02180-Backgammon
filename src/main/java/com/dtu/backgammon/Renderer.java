f6e82d469ab5f5e081cade1bbbdcb3721251e35c
Christian Livbjerg Worsøe <s235111@dtu.dk>
2025-03-12 14:06:33 +0100

    Clean up and fix Renderer

--- src/main/java/com/dtu/backgammon/Renderer.java
+++ src/main/java/com/dtu/backgammon/Renderer.java
@@ -8,63 +8,64 @@ import com.dtu.backgammon.player.Player;
 public class Renderer {
     // See ANSI commands https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
     public static void render(Board board) {
-
-        
-        //setBackgroundColor();
         clearScreen(false);
         printBoard(board);
         printPlayers(board.players);
     }
 
-    static String graybar_color = "40;40;40";
-    static String lighttick_color = "125;106;54";
-    static String darktick_color = "69;55;15";
+    static String barColor = "40;40;40";
+    static String lightTickColor = "125;106;54";
+    static String darkTickColor = "69;55;15";
     private static void printBoard(Board board) {
-        moveCur(0,0);
+        moveCur(0, 0);
         for (int j = 11; j >= 0; j--) { // Print indexes for column
             if (j == 5) { System.out.print("   "); } // Pad grey bar
-            System.out.print(String.format("%3d", j));
+            System.out.printf("%3d", j);
         }
 
         System.out.println();
-        for (int i = 0; i < 5; i++) {
-            for (int j = 11; j >= 0; j--) {
-                if (j == 5) { // Print the white bar
-                    boolean brickInBar = board.barWhite > i;
-                    System.out.print(bcol(graybar_color) + (brickInBar ? fcol(white_color) + " \u25CF " : "   "));
+        for (int row = 0; row < 5; row++) {
+            for (int point = 11; point >= 0; point--) {
+                if (point == 5) { // Print the bar for white
+                    boolean brickInBar = board.barWhite > row;
+                    System.out.print(bcol(barColor) + (brickInBar ? fcol(whiteColor) + " \u25CF " : "   "));
                     resetColor();
                 }
-                Brick brick = board.getBrickAt(j, i);
-                System.out.print(bcol(j%2 == 1 ? lighttick_color : darktick_color) + getBrickPrintStr(brick));
+                Brick brick = board.getBrickAt(point, row);
+                System.out.print(bcol(point % 2 == 1 ? lightTickColor : darkTickColor) + getBrickPrintStr(brick));
             }
+            resetColor();
             System.out.println();
         }
 
-        moveCur(0,6+1);
-        // Print gray bar in the middle of green
-        for (int i = 6; i < 9; i++) {
-            System.out.print(bcol(backgroundcolor) + "   ".repeat(6));
-            System.out.print(bcol(graybar_color) + "   ");
-            System.out.println(bcol(backgroundcolor) + "   ".repeat(6));
+        moveCur(0, 6 + 1);
+        // Print grey bar in the middle of green
+        for (int row = 6; row < 9; row++) {
+            System.out.print(bcol(backgroundColor) + "   ".repeat(6));
+            System.out.print(bcol(barColor) + "   ");
+            System.out.print(bcol(backgroundColor) + "   ".repeat(6));
+            resetColor();
+            System.out.println();
         }
 
-        moveCur(0,6+3+1);
-        for (int i = 4; i >= 0; i--) {
-            for (int j = 12; j < 24; j++) {
-                if (j == 18) { // Print the black bar
-                    boolean brickInBar = board.barBlack > i;
-                    System.out.print(bcol(graybar_color) + (brickInBar ? fcol(black_color) + " \u25CF " : "   "));
+        moveCur(0, 6 + 3 + 1);
+        for (int row = 4; row >= 0; row--) {
+            for (int point = 12; point < 24; point++) {
+                if (point == 18) { // Print the bar for black
+                    boolean brickInBar = board.barBlack > row;
+                    System.out.print(bcol(barColor) + (brickInBar ? fcol(blackColor) + " \u25CF " : "   "));
                     resetColor();
                 }
-                Brick brick = board.getBrickAt(j, i);
-                System.out.print(bcol(j%2 == 1 ? lighttick_color : darktick_color) + getBrickPrintStr(brick));
+                Brick brick = board.getBrickAt(point, row);
+                System.out.print(bcol(point % 2 == 1 ? lightTickColor : darkTickColor) + getBrickPrintStr(brick));
             }
+            resetColor();
             System.out.println();
         }
         resetColor();
         for (int j = 12; j < 24; j++) { // Print indexes for column
             if (j == 18) { System.out.print("   "); } // Pad grey bar
-            System.out.print(String.format("%3d", j));
+            System.out.printf("%3d", j);
         }
         // Temporary move cursor below board
         moveCur(0, 16);
@@ -73,46 +74,40 @@ public class Renderer {
     private static void printPlayers(List<Player> players) {
         System.out.println();
         for (int i = 0; i < players.size(); i++) {
-            System.out.print((i>0 ? " Vs. " : "") + players.get(i).getName());
+            System.out.print((i > 0 ? " Vs. " : "") + players.get(i).getName());
         }
         System.out.println();
     }
 
-    static String white_color = "255;255;255";
-    static String black_color = "0;0;0";
+    static String whiteColor = "255;255;255";
+    static String blackColor = "0;0;0";
     private static String getBrickPrintStr(Brick brick) {
-        switch (brick) {
-            case NONE:
-                return "   ";
-            case WHITE:
-                return fcol(white_color) + " \u25CF ";
-            case BLACK:
-                return fcol(black_color) + " \u25CF ";
-            default:
-                return "";
-        }
+        return switch (brick) {
+            case NONE -> "   ";
+            case WHITE -> fcol(whiteColor) + " \u25CF ";
+            case BLACK -> fcol(blackColor) + " \u25CF ";
+        };
     }
-        
-    static String backgroundcolor = "17;125;7";
+
+    static String backgroundColor = "17;125;7";
     private static void setBackgroundColor() {
-        System.out.print(esc + "[m" + bcol(backgroundcolor));
+        System.out.print(ESC + "[m" + bcol(backgroundColor));
     }
     private static void clearScreen(boolean keepColor) {
-        System.out.print((keepColor ? (esc + "[0m") : "" ) + esc + "[2J");
+        System.out.print((keepColor ? (ESC + "[m") : "") + ESC + "[2J");
     }
     private static String bcol(String rgbcol) {
-        return esc + "[48;2;" + rgbcol + "m";
+        return ESC + "[48;2;" + rgbcol + "m";
     }
     private static String fcol(String rgbcol) {
-        return esc + "[38;2;" + rgbcol + "m";
+        return ESC + "[38;2;" + rgbcol + "m";
     }
     private static void resetColor() {
-        System.out.print(esc + "[0m");
+        System.out.print(ESC + "[m");
     }
     private static void moveCur(int x, int y) {
-        System.out.print(esc + String.format("[%d;%dH", y, x));
+        System.out.print(ESC + String.format("[%d;%dH", y, x));
     }
 
-
-    private static String esc = "\u001b";
+    private static final char ESC = '\u001B';
 }
